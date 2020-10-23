f25edfc821acb7ff5e9f1c1454e83339813b29db
KuzmichevAlex <kuzmichevadata@gmail.com>
2020-10-23 16:54:37 +0300

    Initial Commit

---
+++ color_mixer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  color_mixer
+//
+//  Created by Александр Кузьмичев on 23.10.2020.
+//  Copyright © 2020 Александр Кузьмичев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
